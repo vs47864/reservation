4012b7365f17c03453b965e31c0c6fadc241840e
vs47864 <vs47864@gmail.com>
2017-08-24 17:02:30 +0200

    Event

--- src/main/java/de/fraunhofer/iosb/services/impl/TermServiceImplementation.java
+++ src/main/java/de/fraunhofer/iosb/services/impl/TermServiceImplementation.java
@@ -6,7 +6,9 @@ import de.fraunhofer.iosb.entity.User;
 import de.fraunhofer.iosb.entity.key.TermId;
 import de.fraunhofer.iosb.repository.TermRepository;
 import de.fraunhofer.iosb.repository.UserRepository;
+import de.fraunhofer.iosb.representation.TermDetailsResponse;
 import de.fraunhofer.iosb.representation.TermsResponse;
+import de.fraunhofer.iosb.representation.UserRepresentation;
 import de.fraunhofer.iosb.services.TermService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -41,10 +43,25 @@ public class TermServiceImplementation implements TermService
         {
             for(Term term : room.getTerms())
             {
-                TermsResponse termsResponse = new TermsResponse(term.getTermID().getStartDate(), term.getTermID().getEndDate(), room.getRoomID(), term.getTitle());
+                TermsResponse termsResponse = new TermsResponse(term.getTermID().getStartDate(), term.getTermID().getEndDate(), room.getName(), term.getTitle(), room.getRoomID());
                 terms.add(termsResponse);
             }
         }
         return terms;
+    }
+
+    @Override
+    public TermDetailsResponse getTerm(TermsResponse term) {
+        TermId termId = new TermId(term.getStartDate(), term.getEndDate(), term.getRoomId());
+        Term term1 = termRepository.findOne(termId);
+        List<UserRepresentation> userRepresentations = new ArrayList<>();
+        for (User user : term1.getUsers())
+        {
+            UserRepresentation userRepresentation = new UserRepresentation(user.getName() + user.lastname, user.username);
+            userRepresentations.add(userRepresentation);
+        }
+        UserRepresentation initUser = new UserRepresentation(term1.getUser().getName() + term1.getUser().lastname, term1.getUser().getUsername());
+        TermDetailsResponse termDetailsResponse =new TermDetailsResponse(term, initUser, userRepresentations);
+        return termDetailsResponse;
     }
 }
